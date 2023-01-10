bf0f1f155ef4aa9a7a28066a7fef62ef2bf17294
Ykong <66518156+Ykong1337@users.noreply.github.com>
2023-01-10 10:24:00 +0800

    Create Sys_Proxy.cmd

---
+++ win-proxy-switch/Sys_Proxy.cmd
@@ -0,0 +1,16 @@
+@echo off
+for /f "tokens=1,2,* " %%i in ('REG QUERY "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable ^| find /i "ProxyEnable"') do (set /A ProxyEnableValue=%%k)
+
+if %ProxyEnableValue% equ 0 (
+    reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f >nul 2>nul
+    reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /d "127.0.0.1:7890" /f >nul 2>nul
+    echo System_Proxy Open
+) else if %ProxyEnableValue% equ 1 (
+    reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f >nul 2>nul
+    reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /d "" /f >nul 2>nul
+    echo System_Proxy Close
+)
+
+ping 127.1 -n 3 >nul
+
+exit
