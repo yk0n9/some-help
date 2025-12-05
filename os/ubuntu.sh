4d39c7e241f6599f9e002e99c52468b6887d82e8
Ykong <yk0n9@outlook.com>
2025-12-05 09:10:16 +0800

    Remove sudo from commands in ubuntu.sh

--- os/ubuntu.sh
+++ os/ubuntu.sh
@@ -1,8 +1,8 @@
-sudo sed -i 's@//.*archive.ubuntu.com@//mirrors.ustc.edu.cn@g' /etc/apt/sources.list
-sudo sed -i 's/security.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
+sed -i 's@//.*archive.ubuntu.com@//mirrors.ustc.edu.cn@g' /etc/apt/sources.list
+sed -i 's/security.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
 
-sudo apt update -y
-sudo apt install gcc git vim zsh curl neofetch konsole gnome-tweaks -y
+apt update -y
+apt install gcc git vim zsh curl neofetch konsole gnome-tweaks -y
 
-sudo chsh -s /bin/zsh ${USER}
+chsh -s /bin/zsh ${USER}
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
