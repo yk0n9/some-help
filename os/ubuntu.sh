8a4f6c09b601a43c3cc1d283056366e55def9e7a
Ykong <yk0n9@outlook.com>
2025-12-05 09:06:11 +0800

    Update package installation in ubuntu.sh
    Added konsole and gnome-tweaks to the installation list.

--- os/ubuntu.sh
+++ os/ubuntu.sh
@@ -2,7 +2,7 @@ sudo sed -i 's@//.*archive.ubuntu.com@//mirrors.ustc.edu.cn@g' /etc/apt/sources.
 sudo sed -i 's/security.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
 
 sudo apt update -y
-sudo apt install gcc git vim zsh curl neofetch -y
+sudo apt install gcc git vim zsh curl neofetch konsole gnome-tweaks -y
 
 sudo chsh -s /bin/zsh ${USER}
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
