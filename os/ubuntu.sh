73a2884ccb8cea26932680fa0de0b38e3ccdfad9
Ykong <yk0n9@outlook.com>
2025-12-16 16:34:14 +0800

    Update package installation and input method configuration
    Replaced installation of konsole and gnome-tweaks with ibus and ibus-libpinyin. Configured input method and changed default shell to zsh.

--- os/ubuntu.sh
+++ os/ubuntu.sh
@@ -2,7 +2,9 @@ sed -i 's@//.*archive.ubuntu.com@//mirrors.ustc.edu.cn@g' /etc/apt/sources.list
 sed -i 's/security.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
 
 apt update -y
-apt install gcc git vim zsh curl neofetch konsole gnome-tweaks -y
+apt install gcc git vim zsh curl neofetch ibus ibus-libpinyin -y
+
+im-config -n ibus
 
 chsh -s /bin/zsh ${USER}
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
