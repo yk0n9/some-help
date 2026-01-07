d637122aa5a7128dd17be6e3d8048afc81dc91df
Ykong <yk0n9@outlook.com>
2026-01-07 20:14:32 +0800

    Add files via upload

---
+++ .wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require('wezterm')
+
+local config = wezterm.config_builder()
+
+if wezterm.target_triple:find("windows") then
+    config.default_prog = { 'powershell.exe' }
+end
+
+config.window_background_opacity = 0.7
+config.font_size = 10.0
+config.color_scheme = 'catppuccin-frappe'
+config.font = wezterm.font("Monaspace Neon")
+
+return config
