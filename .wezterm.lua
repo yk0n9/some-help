f2b33bb4e7a1e33c620ca1edd6c972262644b5cd
Ykong <yk0n9@outlook.com>
2026-02-26 19:04:07 +0800

    Change font to 'Comic Mono' in wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -9,6 +9,7 @@ end
 config.window_background_opacity = 0.7
 config.font_size = 10.0
 config.color_scheme = 'catppuccin-frappe'
-config.font = wezterm.font("Monaspace Neon")
+config.font = wezterm.font("Comic Mono")
 
 return config
+
